2f638fe7fd3a7b0649858848987133a582c1ee33
veselin-beev <veselinbeev@gmail.com>
2021-03-24 01:09:38 +0200

    desktop version ready
    TODO:
    *check scss
    *check mobile version

---
+++ src/js/slick.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$(".slick-slider").slick({
+		dots: true,
+		// infinite: true,
+		arrows: false,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+});
